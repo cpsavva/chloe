e8b14628e7b0a0f7036fc5f2f731187b9116ba7a
cpsavva <csoriasavva@gmail.com>
2017-02-02 18:30:50 -0500

    twitter keys

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'GvJiQHq85XStosXU3088Ie5Ap',
+  consumer_secret: 'n54i5S8hmEAXx2gRTWSHY6joFF1Zj6R1poLiN6dQdQcmcsLzUf',
+  access_token_key: '788830650375741440-kscfQJzkabYMXcypJq1GKm3PdbIqdw5',
+  access_token_secret: 'taOd0htTfLtVCGd8YLw8M4jG9C8TFAoyCLqZSBBEUe7LO',
+}
+
+
+
