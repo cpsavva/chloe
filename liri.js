49abb56af3307c58f15040323e77fa728119a6de
cpsavva <csoriasavva@gmail.com>
2017-02-06 22:26:41 -0500

    tweets function

--- liri.js
+++ liri.js
@@ -15,5 +15,34 @@ when the person selects movie info, prompt them to enter movie title
 
 what do i need?
 spotify : https://api.spotify.com/v1/search?query=thriller&type=track&market=US&offset=0&limit=1
+
+
 */
+
+const keys = require("./keys");
+const spotify = require("spotify");
+const Twitter = require("twitter");
+
+var client = new Twitter({
+  consumer_key: keys.twitterKeys.consumer_key,
+  consumer_secret: keys.twitterKeys.consumer_secret,
+  access_token_key: keys.twitterKeys.access_token_key,
+  access_token_secret: keys.twitterKeys.access_token_secret
+});
+
+
+function myTweet(){
+	var params = {screen_name: 'cpsavva'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	  	for (i = 0; i< tweets.length; i++){
+	    console.log(tweets[i].text);
+		}  	
+	  }
+
+	});
+};
+
+
+
 
