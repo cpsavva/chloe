7b8cbe6b731e4474b38efa11abe5dc2d15ce6b1f
cpsavva <csoriasavva@gmail.com>
2017-02-06 21:45:24 -0500

    psedocoding and installed npms

---
+++ liri.js
@@ -0,0 +1,19 @@
+
+//pseudocoding
+/*first i want to run liri.js and i question will pop up?
+How can I help? with three options:
+1) my tweets
+2) spotify this song
+3) movie info
+
+when the person selects the my tweets run mytweets ();
+when the person selects spotify this song, prompt them to enter song title 
+	then run spotify();(might need to reverse this ie run spotify then prompt)
+when the person selects movie info, prompt them to enter movie title
+	then run movie title (might need to reverse this ie run movie then prompt)
+
+
+what do i need?
+spotify : https://api.spotify.com/v1/search?query=thriller&type=track&market=US&offset=0&limit=1
+*/
+
